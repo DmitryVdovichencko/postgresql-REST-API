c967c9c42ddcdbdf8310a73e1e5a6a23d9526219
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-10-11 17:01:39 +0500

    formatting code with prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import pool from "./config";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import pool from './config';
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const getBooks = (request, response) => {
-  pool.query("SELECT * FROM books", (error, results) => {
+  pool.query('SELECT * FROM books', (error, results) => {
     if (error) {
       throw error;
     }
@@ -22,19 +22,19 @@ const addBook = (request, response) => {
   const { author, title } = request.body;
 
   pool.query(
-    "INSERT INTO books (author, title) VALUES ($1, $2)",
+    'INSERT INTO books (author, title) VALUES ($1, $2)',
     [author, title],
     error => {
       if (error) {
         throw error;
       }
-      response.status(201).json({ status: "success", message: "Book added." });
+      response.status(201).json({ status: 'success', message: 'Book added.' });
     }
   );
 };
 
 app
-  .route("/books")
+  .route('/books')
   // GET endpoint
   .get(getBooks)
   // POST endpoint
