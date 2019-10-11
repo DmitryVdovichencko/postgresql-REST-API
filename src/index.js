48d688e74f4b0de5cab16dc9e33c49ad0a77ed2c
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-10-11 16:52:21 +0500

    formatting code with prettier

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import pool from './config'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import pool from "./config";
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const getBooks = (request, response) => {
-  pool.query('SELECT * FROM books', (error, results) => {
+  pool.query("SELECT * FROM books", (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-  })
-}
+    response.status(200).json(results.rows);
+  });
+};
 
 const addBook = (request, response) => {
-  const { author, title } = request.body
+  const { author, title } = request.body;
 
-  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-    if (error) {
-      throw error
+  pool.query(
+    "INSERT INTO books (author, title) VALUES ($1, $2)",
+    [author, title],
+    error => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ status: "success", message: "Book added." });
     }
-    response.status(201).json({ status: 'success', message: 'Book added.' })
-  })
-}
+  );
+};
 
 app
-  .route('/books')
+  .route("/books")
   // GET endpoint
   .get(getBooks)
   // POST endpoint
-  .post(addBook)
+  .post(addBook);
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening on 3002`)
-})
+  console.log(`Server listening on 3002`);
+});
