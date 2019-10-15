5d36a197ac2ebb4f9cc7508390e0e5f9a25e9972
Dmitry Vdovichenko <kenar89@gmail.com>
2019-10-15 16:22:13 +0500

    added init for db

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../src/app';
 
 describe('Server Endpoints', () => {
-  it('Gets the test endpoint', async (done) => {
+  it('Gets the test endpoint', async done => {
     // Sends GET Request to /test endpoint
 
     await request(app)
