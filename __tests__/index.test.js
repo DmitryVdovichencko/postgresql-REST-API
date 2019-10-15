2bb0fc669900121668d7047da4d8351af5cb1796
Dmitry Vdovichenko <kenar89@gmail.com>
2019-10-15 11:39:18 +0500

    formatting code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,12 @@
-import request from 'supertest'
-import app from '../src/index'
-describe('Server Endpoints', () => {
-it("Gets the test endpoint", async () => {
-    // Sends GET Request to /test endpoint
-    
-    const response = await request(app)
-    .get('/books')
-    
-    expect(response.statusCode).toEqual(200)
+import request from 'supertest';
+import app from '../src/index';
 
-  })
-})
+describe('Server Endpoints', () => {
+  it('Gets the test endpoint', async () => {
+    // Sends GET Request to /test endpoint
+
+    const response = await request(app).get('/books');
+
+    expect(response.statusCode).toEqual(200);
+  });
+});
