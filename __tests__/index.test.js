957a15b8f4a774ba5694df6f9bc836c7811f5d52
Dmitry Vdovichenko <kenar89@gmail.com>
2019-10-18 16:10:18 +0500

    post test added

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,28 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('Server Endpoints', () => {
-  it('Gets the test endpoint', async (done) => {
+describe('Get books from server', () => {
+  it('Gets the books endpoint', async (done) => {
     // Sends GET Request to /test endpoint
-
     await request(app)
       .get('/books/')
       .expect(200);
+
+    done();
+  });
+});
+
+describe('Send book to server', () => {
+  it('Gets the books endpoint', async (done) => {
+    // Sends GET Request to /test endpoint
+    const newBook = {
+      title: 'Game of Thrones',
+      author: 'George R. R. Martin',
+    };
+    await request(app)
+      .post('/books')
+      .send(newBook)
+      .expect(201);
 
     done();
   });
