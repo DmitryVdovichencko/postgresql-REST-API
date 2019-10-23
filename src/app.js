33cc00dd8e6997f344b1da89ada74b81cd90ef99
Dmitry Vdovichenko <kenar89@gmail.com>
2019-10-23 12:50:29 +0500

    prod settings configured

--- src/app.js
+++ src/app.js
@@ -2,12 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import pool from './config';
+import helmet from 'helmet';
+import compression from 'compression';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // 5 requests,
+});
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 
 const getBooks = (request, response) => {
   pool.query('SELECT * FROM books', (error, results) => {
